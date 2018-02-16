f730fdbb708077db6e67c0181372d6b0d31ee81a
Janell <janellmartin19@my.whitworth.edu>
2018-02-16 09:41:03 -0800

    Initial commit

---
+++ Triangle.cpp
@@ -0,0 +1,32 @@
+//////////////////
+// Janell Martin
+// 2.16.18
+// CS-273-1
+//////////////////
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+#include "Triangle.h"
+
+// NOTE: This function is deliberately incorrect.
+
+bool isTriangle(int a, int b, int c) {
+    
+    // A correct function would return true
+    
+    // if a, b, c are valid sides for a triangle, i.e.
+    
+    // if (a + b > c && a + c > b && b + c > a)
+    
+    // return true;
+    
+    // else
+    
+    // return false;
+    
+    return true; // PLEASE REMOVE THIS STATEMENT WHEN IMPLEMENTING
+    
+}
---
+++ Triangle.h
@@ -0,0 +1,20 @@
+//
+//  Triangle.h
+//  Triangle
+//
+//  Created by Janell Martin on 2/16/18.
+//  Copyright © 2018 Janell Martin. All rights reserved.
+//
+
+
+#ifndef TRIANGLE_H_
+
+#define TRIANGLE_H_
+
+// This is a function prototype
+
+bool isTriangle(int a, int b, int c);
+
+#endif /*TRIANGLE_H_*/
+
+
